c4d1f1a80ced15a5fe3d9aeb64faa0dbb5dfeb77
Erik Fortune <pc@erik.org>
2018-06-04 19:11:23 -0700

    Select output correctly

--- commands/raids/add.js
+++ commands/raids/add.js
@@ -70,7 +70,7 @@ class raid extends commando.Command {
             const [, tier, gym, startTime] = match;
             try {
                 client.raidManager.addEggAbsolute(tier, gym, startTime);
-                message.channel.send(client.raidManager.listFormatted());
+                message.channel.send(client.raidManager.listFormatted((r) => r.tier >= raid.tier));
             }
             catch (e) {
                 client.reportError(message, "!add", e, this.examples[eggStartTimeSampleIndex]);
@@ -83,7 +83,7 @@ class raid extends commando.Command {
             const [, tier, gym, timer] = match;
             try {
                 client.raidManager.addEggCountdown(tier, gym, timer);
-                message.channel.send(client.raidManager.listFormatted());
+                message.channel.send(client.raidManager.listFormatted((r) => r.tier >= tier));
             }
             catch (err) {
                 client.reportError(message, "!add", err, this.examples[eggTimerSampleIndex]);
@@ -98,7 +98,7 @@ class raid extends commando.Command {
 
             try {
                 let raid = client.raidManager.addRaid(boss, gym, timer);
-                message.channel.send(client.raidManager.listFormatted((r) => r.tier >= raid));
+                message.channel.send(client.raidManager.listFormatted((r) => r.tier >= raid.tier));
             }
             catch (err) {
                 let commandSyntax = this.examples[bosswithTimerForSampleIndex] + "\nOR\n" + this.examples[bossWithTimerLeftSampleIndex];
@@ -113,7 +113,7 @@ class raid extends commando.Command {
             const [, boss, gym] = match;
             try {
                 let raid = client.raidManager.setRaidBoss(boss, gym);
-                message.channel.send(client.raidManager.listFormatted((r) => r.tier >= raid));
+                message.channel.send(client.raidManager.listFormatted((r) => r.tier >= raid.tier));
             }
             catch (err) {
                 // If we get this error, the raid is likely not in the active list, so give the
