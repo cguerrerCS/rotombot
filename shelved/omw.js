ace844189ea2a22020526f209cb51dc26a1010ea
DidjaRedo <pc@erik.org>
2018-06-11 22:18:21 -0700

    Merge master to gymChanges branch (#21)
    * Adds !ima to rsvp for raids and report status (#19)
    
    * Started adding rsvp commands
    
    * Checkpoint work in progress.  "!omw" command sort of working
    
    * Display number of inbound & present raiders in gym list
    
    * checkpoint work in progress
    
    * Checkpoint work in progress - rsvp factored out, !omw command seems to work
    
    * Added alternate RSVP syntax with "!iam"
    
    * Merge master to refactor
    
    * Generalize list filters & add city filter to !raids command
    
    * Update help for !raids command
    
    * Add ability to define raid channels that automatically update with a specified raids command
    
    * Automatically save and restore raids state
    
    * Test updates & minor fixes
    
    * Updated tests & logging
    
    * checkpoint work in progress - working on save/restore for rsvp
    
    * Fixed a few save/restore bugs for rsvp
    
    * Tweak boss logic for hatching an egg with only one possibility (#16) (#17)
    
    * Automatically add the raid boss to a hatched egg if there's only one possibility
    
    * Update raid tier based on Boss
    
    Update the tier of the raid if it does not match the boss
    
    * Update raid bosses

---
+++ shelved/omw.js
@@ -0,0 +1,146 @@
+"use strict";
+const commando = require("discord.js-commando");
+
+const omwNoEtaRegex = /^!omw\s+(?:to\s+)?(\w+(?:\s|\w)*)\s*$/i;
+const omwEtaRegex = /^!omw\s+(?:to\s+)?(\w+(?:\s|\w)*)\s+eta\s+((?:\d\d?)|((?:\d?\d):?(?:\d\d)\s*(?:a|A|am|AM|p|P|pm|PM)?)|hatch)\s*$/i;
+const omwStartRegex = /^!omw\s+(?:to\s+)?(\w+(?:\s|\w)*)\s+(?:starting|start)\s*(\w+(?:\s|\w)*)*\s*$/i;
+const omwOtherRegex = /^!omw\s+(?:to\s+)?(\w+(?:\s|\w)*)\s+(never|here|done)\s*$/i;
+
+const omwNoEtaExampleIndex = [2];
+const omwEtaExampleIndexes = [5, 6, 7];
+const omwHereExampleIndex = [10];
+const omwStartExampleIndexes = [13, 14];
+const omwDoneExampleIndex = [17];
+const omwCanceExampleIndex = [20];
+
+//!raids command
+class raids extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "omw",
+            group: "raids",
+            memberName: "omw",
+            description: "RSVP for a raid",
+            examples: [
+                "On my way (no eta):",
+                "  !omw [to] <gym>",
+                "  !omw to painted",
+                "On my way with ETA as absolute or delta:",
+                "  !omw [to] <gym> eta (<minutes>|<time>|hatch)",
+                "  !omw to market eta 20",
+                "  !omw to market eta 1130",
+                "  !omw to market eta hatch",
+                "Arrived at the raid:",
+                "  !omw [to] <gym> here",
+                "  !omw market here",
+                "Started the raid:",
+                "  !omw [to] <gym> start <optional message>",
+                "  !omw luke start",
+                "  !omw soul foods start mystic pika pika bulba",
+                "Finished the raid:",
+                "  !omw [to] <gym> done",
+                "  !omw soul foods done",
+                "Cancel an RSVP",
+                "  !omw [to] <gym> never",
+                "  !omw to luke never",
+            ],
+        });
+    }
+
+    async run(message) {
+        let client = message.client;
+
+        let wantGym = undefined;
+        let raiderInfo = {
+            raiderName: message.member.user.username,
+            gym: undefined,
+            etaTime: undefined,
+            arrivalTime: undefined,
+            startTime: undefined,
+            endTime: undefined,
+            code: undefined,
+        };
+        let helpExamples = 0;
+
+        let match = message.content.match(omwEtaRegex);
+
+        if (match !== null) {
+            console.log(`on my way with eta: ${match}\n`);
+            let [, gymName, wantTime] = match;
+            wantGym = gymName;
+            raiderInfo.etaTime = wantTime;
+            helpExamples = omwEtaExampleIndexes;
+        }
+        else {
+            match = message.content.match(omwStartRegex);
+            if (match !== null) {
+                console.log(`started raid: ${match}\n`);
+                let [, gymName, wantCode] = match;
+                wantGym = gymName;
+                raiderInfo.startTime = new Date();
+                raiderInfo.code = wantCode;
+                helpExamples = omwStartExampleIndexes;
+            }
+            else {
+                match = message.content.match(omwOtherRegex);
+                if (match !== null) {
+                    console.log(`omw other: ${match}`);
+                    let [, gymName, verb] = match;
+                    wantGym = gymName;
+                    switch (verb.toLowerCase()) {
+                        case "here":
+                            raiderInfo.arrivalTime = new Date();
+                            helpExamples = omwHereExampleIndex;
+                            break;
+                        case "done":
+                            raiderInfo.endTime = new Date();
+                            helpExamples = omwDoneExampleIndex;
+                            break;
+                        case "never":
+                            raiderInfo = undefined;
+                            helpExamples = omwCanceExampleIndex;
+                            break;
+                        default:
+                            client.reportError(message, "!omw", `My circuitzzz are tingling! I didn't recognize the verb "${verb}"`);
+                            return;
+                    }
+                }
+                else {
+                    match = message.content.match(omwNoEtaRegex);
+                    if (match === null) {
+                        client.reportError(
+                            message,
+                            "!add",
+                            "My circuitzzz are tingling! I didn't understand that command..."
+                        );
+                        return;
+                    }
+                    console.log(`omw no eta: ${match}`);
+                    let [, gymName] = match;
+                    wantGym = gymName;
+                    helpExamples = omwNoEtaExampleIndex;
+                }
+            }
+        }
+
+        try {
+            if (raiderInfo) {
+                let raid = client.raidManager.addOrUpdateRaider(wantGym, raiderInfo);
+                message.channel.send(`${raid.gym.name}: ${raid.raiders[raiderInfo.name].toString()}`);
+            }
+            else {
+                let raid = client.raidManager.removeRaider(wantGym, message.member.user.username);
+                message.channel.send(`${raid.gym.name}: Cancelled rsvp for ${message.member.user.username}`);
+            }
+        }
+        catch (err) {
+            let help = "";
+            helpExamples.forEach((i) => {
+                help += this.examples[i] + "\n";
+            });
+            client.reportError(message, "!omw", err, help);
+        }
+    }
+}
+
+module.exports = raids;
