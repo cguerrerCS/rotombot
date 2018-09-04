25764b1ca600b5c34e070300915759cead8f4fab
Erik Fortune <pc@erik.org>
2018-09-03 18:14:55 -0700

    Added parser tests, redmond ridge gyms

--- test/unit/cmdRegexSpec.js
+++ test/unit/cmdRegexSpec.js
@@ -19,4 +19,68 @@ describe("cmdRegexHelper", () => {
             expect(match[3]).toBe(test.time);
         });
     });
+
+    it("should not regress the !add egg by timer command", () => {
+        // const addEggByTimerRegex = /^!add\s+(?:(?:L|T|l|t)?(\d+)\s+)?(\w+(?:\s|\w|'|-)*)(?:\s+(?:in)\s*)(\d?\d)\s*?$/i;
+        let cmd = regex.create(["!add", "[tier]", "[gym]", "in", "[timer]"]);
+        [
+            { str: "!add Hoity-toity in 10", tier: undefined, gym: "Hoity-toity", timer: "10" },
+            { str: "!add T3 Blah Blah in 5", tier: "3", gym: "Blah Blah", timer: "5" },
+            { str: "!add T1 St. Jude's in 55", tier: "1", gym: "St. Jude's", timer: "55" },
+        ].forEach((test) => {
+            let match = test.str.match(cmd);
+            expect(match).not.toBeNull();
+            expect(match[1]).toEqual(test.tier);
+            expect(match[2]).toBe(test.gym);
+            expect(match[3]).toBe(test.timer);
+        });
+    });
+
+    it("should not regress the !add boss with timer command", () => {
+        // const addBossWithTimerRegex = /^!add\s*((?:\w|-)+)\s*(?:@|at)\s*(\w+(?:\w|\s|'|-)*)(?:\s+(\d?\d)\s*(?:left))\s*?$/i;
+        let cmd = regex.create(["!add", "[boss]", "(?:@|at)", "[gym]", "[timer]", "left"]);
+        [
+            { str: "!add Boss at Hoity-toity 10 left", boss: "Boss", gym: "Hoity-toity", timer: "10" },
+            { str: "!add Who at Blah Blah 5 left", boss: "Who", gym: "Blah Blah", timer: "5" },
+            { str: "!add Ho-oh at St. Jude's 55 left", boss: "Ho-oh", gym: "St. Jude's", timer: "55" },
+        ].forEach((test) => {
+            let match = test.str.match(cmd);
+            expect(match).not.toBeNull();
+            expect(match[1]).toEqual(test.boss);
+            expect(match[2]).toBe(test.gym);
+            expect(match[3]).toBe(test.timer);
+        });
+    });
+
+    it("should not regress the alternate !add boss with timer command", () => {
+        // const addBossWithTimerAltRegex = /^!add\s*((?:\w|-)+)\s*(?:@|at)\s*(\w+(?:\w|\s|`|-)*)(?:for)(?:\s+(\d?\d)\s*)\s*?$/i;
+        let cmd = regex.create(["!add", "[boss]", "(?:@|at)", "[gym]", "for", "[timer]"]);
+        [
+            { str: "!add Ho-oh at Hoity-toity for 10", boss: "Ho-oh", gym: "Hoity-toity", timer: "10" },
+            { str: "!add Who at Blah Blah for 5", boss: "Who", gym: "Blah Blah", timer: "5" },
+            { str: "!add Ho-oh at St. Jude's for 55", boss: "Ho-oh", gym: "St. Jude's", timer: "55" },
+        ].forEach((test) => {
+            let match = test.str.match(cmd);
+            expect(match).not.toBeNull();
+            expect(match[1]).toEqual(test.boss);
+            expect(match[2]).toBe(test.gym);
+            expect(match[3]).toBe(test.timer);
+        });
+    });
+
+    
+    it("should not regress the alternate !add boss without  timer command", () => {
+        // const addBossNoTimerRegex = /^!add\s*((?:\w|-)+)\s*(?:@|at)\s*(\w+(?:\w|\s|`|-)*)$/i;
+        let cmd = regex.create(["!add", "[boss]", "(?:@|at)", "[gym]"]);
+        [
+            { str: "!add Ho-oh at Hoity-toity", boss: "Ho-oh", gym: "Hoity-toity" },
+            { str: "!add Who at Blah Blah", boss: "Who", gym: "Blah Blah" },
+            { str: "!add Ho-oh at St. Jude's", boss: "Ho-oh", gym: "St. Jude's" },
+        ].forEach((test) => {
+            let match = test.str.match(cmd);
+            expect(match).not.toBeNull();
+            expect(match[1]).toEqual(test.boss);
+            expect(match[2]).toBe(test.gym);
+        });
+    });
 });
